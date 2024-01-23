ce815dbbc3dd3b87631fe29190497d463dde9994
Muskinator <156890781+Muskinator@users.noreply.github.com>
2024-01-23 14:24:56 +0100

    Test push

--- Scene/character_body_2d.gd
+++ Scene/character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -600.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -27,5 +27,5 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	var isLeft = velocity.x < 0
+	var isLeft = velocity.x > 0
 	sprite_2d.flip_h = isLeft
