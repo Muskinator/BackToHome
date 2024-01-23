ca9a83be2951f0d1cad388bab66c2ede6d51a26f
Val-s18 <valentin.renaud188@gmail.com>
2024-01-23 13:45:01 +0100

    menu lvl 1 done

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -4,8 +4,7 @@ class_name MainMenu
 @onready var button_start = $MarginContainer/HBoxContainer/VBoxContainer/Button_Start as Button
 @onready var button_lvl = $MarginContainer/HBoxContainer/VBoxContainer/Button_lvl as Button 
 @onready var button_exit = $MarginContainer/HBoxContainer/VBoxContainer/Button_Exit as Button
-@onready var start_level = preload("res://Scene/Lvl1.tscn") as PackedScene
-const main_level = preload("res://Scene/main_level.tscn") as PackedScene
+
 
 func _ready():
 	button_start.button_down.connect(on_start_press)
@@ -14,10 +13,10 @@ func _ready():
 	
 
 func on_start_press() -> void:
-	get_tree().change_scene_to_packed(start_level)
+	get_tree().change_scene_to_file("res://Scene/Lvl1.tscn")
 	
 func on_level_press() -> void:
-	get_tree().change_scene_to_packed(main_level)
+	get_tree().change_scene_to_file("res://Scene/main_level.tscn")
 	
 func on_exit_press() -> void: 
 	get_tree().quit()
