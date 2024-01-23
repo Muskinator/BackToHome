74b009f65a3928b6a9f456a508910f3411be82a0
Val-s18 <valentin.renaud188@gmail.com>
2024-01-23 11:24:17 +0100

    menu

---
+++ Scene/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+class_name MainMenu
+
+@onready var button_start = $MarginContainer/HBoxContainer/VBoxContainer/Button_Start as Button
+@onready var button_lvl = $MarginContainer/HBoxContainer/VBoxContainer/Button_lvl as Button 
+@onready var button_exit = $MarginContainer/HBoxContainer/VBoxContainer/Button_Exit as Button
+@onready var start_level = preload("res://Scene/Lvl1.tscn") as PackedScene
+
+func _ready():
+	button_start.button_down.connect(on_start_press)
+	button_exit.button_down.connect(on_exit_press)
+
+func on_start_press() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+func on_exit_press() -> void: 
+	get_tree().quit()
+	
