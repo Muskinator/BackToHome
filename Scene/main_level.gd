eeb7b262506578a4198f47ed20bb2e7c8cc3b07c
Val-s18 <valentin.renaud188@gmail.com>
2024-01-23 14:22:59 +0100

    fix last push

--- Scene/main_level.gd
+++ Scene/main_level.gd
@@ -1,8 +1,10 @@
 extends Control
 @onready var button_lvl_1 = $MarginContainer/HBoxContainer/VBoxContainer/Button_lvl_1 as Button
 
+const lvl_1 = preload("res://Scene/Lvl1.tscn") as PackedScene
 
 func _ready():
 	button_lvl_1.button_down.connect(btn_1)
 
 func btn_1() -> void:
+	get_tree().change_scene_to_packed(lvl_1)
