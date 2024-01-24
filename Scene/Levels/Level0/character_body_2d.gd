1bf1d36e243fb80972fa49033624aa351218f466
Muskinator <156890781+Muskinator@users.noreply.github.com>
2024-01-24 17:00:40 +0100

    Personnage animation

--- Scene/Levels/Level0/character_body_2d.gd
+++ Scene/Levels/Level0/character_body_2d.gd
@@ -16,11 +16,17 @@ func handleCollision ():
 		print_debug(collider.name)
 		
 func _physics_process(delta):
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "Running"
+	else :
+		sprite_2d.animation = "Default"
+
 	handleCollision()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		sprite_2d.animation = "Jump"
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
