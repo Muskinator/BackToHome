4421616c7af9d9a23e4aa7cb3a513a364166e0fb
Val-s18 <valentin.renaud188@gmail.com>
2024-01-24 13:32:04 +0100

    item / projet

--- Scene/lvl_portalFinal.gd
+++ Scene/lvl_portalFinal.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-
-
 func _on_body_entered(body):
 	if ( body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
+		get_tree().change_scene_to_file("res://Scene/Page_win.tscn")
