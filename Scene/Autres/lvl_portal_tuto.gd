0eea452f86446a48c04a6c220d4b4ba675bf59e4
Val-s18 <valentin.renaud188@gmail.com>
2024-01-25 11:12:32 +0100

    tuto / lvl 2

--- Scene/Autres/lvl_portal_tuto.gd
+++ Scene/Autres/lvl_portal_tuto.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if ( body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
+		get_tree().change_scene_to_file("res://Scene/Pages_Utilisateur/main_menu.tscn")
