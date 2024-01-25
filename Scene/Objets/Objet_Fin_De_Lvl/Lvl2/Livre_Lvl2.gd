d2a42215a9bb4385618392e75184b922dccad33e
Val-s18 <valentin.renaud188@gmail.com>
2024-01-25 10:54:57 +0100

    lvl 2 debug

--- Scene/Objets/Objet_Fin_De_Lvl/Lvl2/Livre_Lvl2.gd
+++ Scene/Objets/Objet_Fin_De_Lvl/Lvl2/Livre_Lvl2.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if ( body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_file("res://Scene/Lvl3.tscn")
+		get_tree().change_scene_to_file("res://Scene/Levels/Level3/Lvl3.tscn")
