e66e79286c52365d6ec1176e5c985c1081b431ad
Muskinator <156890781+Muskinator@users.noreply.github.com>
2024-01-25 09:48:07 +0100

    Corrections bug + ajouts coeurs

--- Scene/Objets/collectable.gd
+++ Scene/Objets/collectable.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
 @onready var game_manager = %"Game Manager"
-@onready var collected = $Collected
 
 
 
 func _on_body_entered(body: Node ) -> void:
 	if (body.name == "CharacterBody2D"):
-		collected.playing = true
 		queue_free()
 		game_manager.add_points()
 
